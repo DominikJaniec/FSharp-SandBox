c9eb9bb8f3bd3c05b79825e337914b8ec884d62f
Dominik Janiec <dominikjaniec@gmail.com>
2019-12-01 23:22:02 +0100

    Project: build script

--- build.fsx
+++ build.fsx
@@ -1,28 +1,47 @@
+open Fake.IO
 #r "paket:
 nuget Fake.Core.Target
-//" // include Fake modules, see Fake modules section
+nuget Fake.DotNet.Cli
+//"
 
 open Fake.Core
+open Fake.Core.TargetOperators
+open Fake.DotNet
+
+
+module GathererBuild =
+
+  let private paths =
+    let src name =
+      Path.combine "src" name
+
+    {| Gatherer = src "Gatherer"
+    |}
+
+
+  let execute (param: TargetParameter) =
+    Trace.log " --- Building the app --- "
+    DotNet.build id paths.Gatherer
+    ()
+
 
 // *** Define Targets ***
-Target.create "Clean" (fun _ ->
+
+Target.create "Clean" <| fun _ ->
   Trace.log " --- Cleaning stuff --- "
-)
 
-Target.create "Build" (fun _ ->
-  Trace.log " --- Building the app --- "
-)
+Target.create "Build"
+  GathererBuild.execute
 
-Target.create "Deploy" (fun _ ->
-  Trace.log " --- Deploying app --- "
-)
+Target.create "ReBuild" <| fun _ ->
+  Trace.log " --- Executing ReBuild ---"
 
-open Fake.Core.TargetOperators
 
 // *** Define Dependencies ***
-"Clean"
-  ==> "Build"
-  ==> "Deploy"
+
+"Clean" ?=> "Build"
+"Clean" ==> "ReBuild"
+"Build" ==> "ReBuild"
 
 // *** Start Build ***
-Target.runOrDefault "Deploy"
+Target.runOrDefault "Build"
